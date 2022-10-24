b15926247474d987321e0bf7125ad106405fa352
Manoel Mateus <mateusmanoel@lampp-it.com.br>
2022-10-23 23:47:06 -0300

    verificando dias da transações no controler

---
+++ dist/js/models/dias-semana.js
@@ -0,0 +1,10 @@
+export var DiaSemanaEnum;
+(function (DiaSemanaEnum) {
+    DiaSemanaEnum[DiaSemanaEnum["DOMINGO"] = 0] = "DOMINGO";
+    DiaSemanaEnum[DiaSemanaEnum["SEGUNDA"] = 1] = "SEGUNDA";
+    DiaSemanaEnum[DiaSemanaEnum["TERCA"] = 2] = "TERCA";
+    DiaSemanaEnum[DiaSemanaEnum["QUARTA"] = 3] = "QUARTA";
+    DiaSemanaEnum[DiaSemanaEnum["QUINTA"] = 4] = "QUINTA";
+    DiaSemanaEnum[DiaSemanaEnum["SEXTA"] = 5] = "SEXTA";
+    DiaSemanaEnum[DiaSemanaEnum["SABADO"] = 6] = "SABADO";
+})(DiaSemanaEnum || (DiaSemanaEnum = {}));
